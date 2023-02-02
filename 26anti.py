8b54d5e1905dededb7612a80a834a2ef8df347a1
jgarciaarango <jesusagarcia2410@gmail.com>
2023-02-02 14:37:04 -0500

    update

--- 26anti.py
+++ 26anti.py
@@ -6,7 +6,18 @@
 # Variation: try this with the range() function and slice syntax
 
 dna = 'ACTGAAAAAAAAAAA'
-
+antiparallel = ''
+for i in range(len(dna)):
+	nt = dna[i] 
+	if nt == 'A' :
+		antiparallel += 'T'
+	elif nt == 'T':
+		antiparallel += 'A'
+	elif nt == 'G':
+		antiparallel += 'C'
+	else:
+		antiparallel += 'G' 
+print(antiparallel[::-1])
 
 """
 python3 26anti.py
